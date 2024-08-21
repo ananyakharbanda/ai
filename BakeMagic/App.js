3b4c2ef5fd72deb21a9ebacce772ff58a022de72
ananya kharbanda <ananyakharbanda28@gmail.com>
2024-08-21 22:21:47 +0800

    added splash screen

--- BakeMagic/App.js
+++ BakeMagic/App.js
@@ -4,10 +4,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen';
 import CameraScreen from './components/CameraScreen';
 import RecipeScreen from './components/RecipeScreen'; // Import the new screen
+import { useEffect } from 'react';
+import BootSplash from 'react-native-bootsplash';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // No tasks to perform, so we can directly hide the BootSplash
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
